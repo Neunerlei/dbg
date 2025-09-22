6fd1f3a2dc3c911ff747d70de8ee02aa59c75eaa
neunerlei <ich@neunerlei.eu>
2025-09-22 13:10:43 +0200

    feat: load config more reliably in cli mode

--- src/Classes/Util/ConfigLoader.php
+++ src/Classes/Util/ConfigLoader.php
@@ -10,30 +10,38 @@ class ConfigLoader
     public function load(): void
     {
         $directories = [];
-
+        
         // Load well known keys in the $_SERVER super-globals array as potential directory
         foreach (['DDEV_COMPOSER_ROOT', 'PWD', 'DBG_CONFIG_DIR'] as $wellKnownServerKey) {
             if (isset($_SERVER[$wellKnownServerKey])) {
                 $directories[] = $_SERVER[$wellKnownServerKey];
             }
         }
-
+        
+        $loadRecursive = function (string $dir) use (&$loadRecursive, &$directories) {
+            if (is_readable($dir) && $dir !== '/') {
+                $directories[] = $dir;
+                $loadRecursive(dirname($dir));
+            }
+        };
+        
         // Load all parents of the document root
         if (isset($_SERVER['DOCUMENT_ROOT'])) {
-            $dir = $_SERVER['DOCUMENT_ROOT'];
-            while (is_readable($dir) && $dir !== '/') {
-                $directories[] = $dir;
-                $dir = dirname($dir);
-            }
+            $loadRecursive($_SERVER['DOCUMENT_ROOT']);
         }
-
+        
+        // Load all parents of the current working directory
+        if (function_exists('getcwd') && getcwd() !== false && !in_array(getcwd(), $directories, true)) {
+            $loadRecursive(getcwd());
+        }
+        
         foreach ($directories as $dir) {
             $candidates = [
                 $this->joinNames($dir, 'dbg.config.php'),
                 $this->joinNames($this->joinNames($dir, '.dbg'), 'dbg.config.php'),
                 $this->joinNames($this->joinNames($dir, '.dbg'), 'config.php'),
             ];
-
+            
             foreach ($candidates as $configFile) {
                 if (is_readable($configFile)) {
                     $this->loadFile($configFile);
@@ -41,12 +49,12 @@ class ConfigLoader
             }
         }
     }
-
+    
     protected function joinNames(string $dir, string $name): string
     {
         return rtrim($dir, '\\/') . '/' . $name;
     }
-
+    
     protected function loadFile(string $filename): void
     {
         $that = new class {
